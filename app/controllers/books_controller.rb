df6f37e930b77d7a51e537209141b248eb0ac111
sowa-takei <souwasora@gmail.com>
2021-08-07 15:14:48 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,20 @@ class BooksController < ApplicationController
     @book  = Book.new(book_params)
     @book .user_id = current_user.id
     @book .save
-    redirect_to books_path
+    @book = Book.new
+    redirect_to user_path(id: current_user)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
     @book = Book.find(params[:id])
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  protect_from_forgery
+  
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
+    @book = Book.new
   end
 
   def edit
